c435619b5b5b1bb91fa96bb162dc5180c91a54df
Christian Jarjat <jarjatc@gmail.com>
2023-03-15 12:13:57 +0000

    first commit

---
+++ mySQL/BullsCowsDB.sql
@@ -0,0 +1,8 @@
+-- Defines the data base and creates the tables in it
+
+
+-- Drops any existing databases, then rebuilds a database with the same name. It will then be ready to use
+
+DROP DATABASE IF EXISTS bullscows;
+CREATE DATABASE bullscows;
+USE bullscows;
